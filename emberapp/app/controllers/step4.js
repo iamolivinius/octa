1d81e2b522a545bae0b3425b101416c1777ab863
Felix Epp <felix.epp@sit.fraunhofer.de>
2015-01-08 20:17:27 +0100

    Cleanup obsolete code in controllers and templates

--- emberapp/app/controllers/step4.js
+++ emberapp/app/controllers/step4.js
@@ -3,23 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   needs: ['step2'],
-  containers: Ember.computed.alias('controllers.step2.containers'),
-
-  init: function() {
-  },
-
-  actions: {}
+  containers: Ember.computed.alias('controllers.step2.containers')
 
 });
-
-/*//Taken from inputs.js
-$(document).on('input propertychange', '#process textarea', function () {
-  // Get container name
-  var container = $(this).attr('data-container');
-
-  // Set processing function
-  if (observer.containers[container] === undefined) {
-    observer.containers[container] = {};
-  }
-  observer.containers[container].process = $(this).val();
-});*/
